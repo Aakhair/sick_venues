649489b343e19f7d7466f562275bacbdb5554f22
Natalie  Narkprasert <natalie.nark@gmail.com>
2022-08-22 17:28:18 +0100

    seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts 'Creating 10 fake users...'
+10.times do
+user = User.new(
+  email: Faker::Internet.email,
+  password: Faker::Internet.password(min_length: 8),
+  full_name: Faker::Name.name,
+)
+user.save!
+end
+puts '10 Users have been created!'
+
+
+puts 'Creating 10 fake venues...'
+title = ['Torre de Belém', 'Pena Palace', 'Castle of Santa Maria da Feira', 'Ribeira Palace', 'Castelo de São Jorge', 'Fontainebleu', 'Mont Saint-Michel', 'Sagrada de Familia', 'Château de Chambord', 'Palace of Versailles']
+category = ['badass wedding', 'intense ravers', 'crazy hippies', 'outdoor concert', 'epic birthday', 'messy graduation', 'dangerous corporate outing', 'unshitty family reunion', 'dog lovers']
+
+10.times do
+venue = Venue.new(
+title: title.sample,
+description: Faker::Hipster.paragraph_by_chars(characters: 256, supplemental: false),
+location: Faker::Address.full_address,
+capacity: rand(140..2400),
+category: category.sample,
+price: rand(800..10000),
+)
+venue.save!
+end
+puts '10 Venues have been created!'
