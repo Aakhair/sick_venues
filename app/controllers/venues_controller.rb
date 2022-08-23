329a9680d68d93a49ab4206b3e029b4abbd78c4a
Anna-Lena Hof <hofannalena@gmail.com>
2022-08-23 11:58:14 +0100

    show action and view added

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,2 +1,12 @@
 class VenuesController < ApplicationController
+  before_action :set_venue, only: [:show]
+
+  def show
+    @venue = Venue.find(params[:id])
+  end
+
+  private
+  def set_venue
+    @venue = Venue.find(params[:id])
+  end
 end
