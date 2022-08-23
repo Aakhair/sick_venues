407f33360bb53e41cbae3e614326cf98f560cdde
Abdullah Alblooshi <aakhair12@gmail.com>
2022-08-23 17:17:48 +0100

    some comment added in case we have an admin or destroy

--- app/policies/venue_policy.rb
+++ app/policies/venue_policy.rb
@@ -1,5 +1,6 @@
 class VenuePolicy < ApplicationPolicy
   class Scope < Scope
+    # un comment if we have an admin
     # NOTE: Be explicit about which records you allow access to!
     # def resolve
     #   scope.all
