ab9e0645aff39d460be525008ad54ac48964b9e1
Abdullah Alblooshi <aakhair12@gmail.com>
2022-08-23 15:01:26 +0100

    drop down for Category added

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -2,6 +2,7 @@ class Venue < ApplicationRecord
   belongs_to :user
   has_many :reservations
   has_one_attached :photo
+  CATEGORIES = ['badass wedding', 'intense ravers', 'crazy hippies', 'outdoor concert', 'epic birthday', 'messy graduation', 'dangerous corporate outing', 'unshitty family reunion', 'dog lovers']
 
   validates :title, presence: true
 end
