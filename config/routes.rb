359ecbe1767d192b288561e62367559e61ec9382
joaquimluis <joaquim.mrvl@gmail.com>
2022-08-23 15:01:54 +0100

    added testimonials section with carousel

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+
+  get "/landing-page", to: "pages#landing_page"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
