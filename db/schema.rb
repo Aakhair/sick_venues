950ca7e4b2b11f7b737fa6e30d46b521481644b4
Abdullah Alblooshi <aakhair12@gmail.com>
2022-08-24 10:50:41 +0100

    added columns to venues

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_23_103851) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_24_094944) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -75,6 +75,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_23_103851) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latiude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_venues_on_user_id"
   end
 
