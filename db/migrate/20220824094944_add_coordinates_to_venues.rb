b8e00b3c1390f0b19bfb91c2d8574b7507f209fb
Abdullah Alblooshi <aakhair12@gmail.com>
2022-08-24 11:32:04 +0100

    bug fixed

--- db/migrate/20220824094944_add_coordinates_to_venues.rb
+++ db/migrate/20220824094944_add_coordinates_to_venues.rb
@@ -1,6 +1,6 @@
 class AddCoordinatesToVenues < ActiveRecord::Migration[7.0]
   def change
-    add_column :venues, :latiude, :float
+    add_column :venues, :latitude, :float
     add_column :venues, :longitude, :float
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -75,7 +75,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_094944) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float "latiude"
+    t.float "latitude"
     t.float "longitude"
     t.index ["user_id"], name: "index_venues_on_user_id"
   end
